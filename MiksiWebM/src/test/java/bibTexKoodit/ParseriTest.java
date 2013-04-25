bc4fe3acc69afddd62ff69f57533c6addbd3f41a
nakkje <niklas.lillqvist@helsinki.fi>
2013-04-25 13:28:04 +0300

    muotojen testaus

--- MiksiWebM/src/test/java/bibTexKoodit/ParseriTest.java
+++ MiksiWebM/src/test/java/bibTexKoodit/ParseriTest.java
@@ -39,7 +39,7 @@ public class ParseriTest {
     //
 
     @Test
-    public void syotetytParametritBibTexmuotoon() {
+    public void syotetytParametritBibTexmuotoonKirja() {
         List<HashMap> kirjat = new ArrayList();
         String type = "book";
         String id = "JB3Q5";
@@ -50,6 +50,7 @@ public class ParseriTest {
         String tulos;
 
         Parseri parser = new Parseri(kirjat);
+        parser.bibTexTulos = "";
         tulos = parser.muutaBibTexMuotoonKirja(type, id, author, title, year, publisher);
 
 
@@ -62,22 +63,87 @@ public class ParseriTest {
         odotettuTulos = odotettuTulos.concat("  publisher = {" + publisher + "}, \n");
         odotettuTulos = odotettuTulos.concat("} \n");
 
-        
-        System.out.println("ODOTETTU:" + odotettuTulos);
-        System.out.println("TULOS:" + tulos);
         assertEquals(odotettuTulos, tulos);
     }
 
+    @Test
+    public void syotetytParametritBibTexmuotoonArticle() {
+        List<HashMap> kirjat = new ArrayList();
+        String type = "article";
+        String id = "JB3Q5";
+        String author = "Vihavainen";
+        String title = "Clean monkeys";
+        String year = "2008";
+        String month = "january";
+        String journal = "Nature";
+        String volume = "13";
+        String pages = "250-258";
+        String address = "Helsinki";
+        String publisher = "jokuvaan";
+        String tulos;
+
+        Parseri parser = new Parseri(kirjat);
+        parser.bibTexTulos = "";
+        tulos = parser.muutaBibTexMuotoonArtikkeli(type, id, author, title, year, month, journal, volume, pages, address, publisher);
+
+
+        String odotettuTulos = "";
+        odotettuTulos = odotettuTulos.concat("@" + type + "{");
+        odotettuTulos = odotettuTulos.concat(id + ", \n");
+        odotettuTulos = odotettuTulos.concat("  author = {" + author + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  title = {" + title + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  year = {" + year + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  month = {" + month + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  journal = {" + journal + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  volume = {" + volume + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  pages = {" + pages + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  address = {" + address + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  publisher = {" + publisher + "}, \n");
+        odotettuTulos = odotettuTulos.concat("} \n");
+
+        assertEquals(odotettuTulos, tulos);
+    }
+    @Test
+    public void syotetytParametritBibTexmuotoonInproc() {
+        List<HashMap> kirjat = new ArrayList();
+        String type = "inproceedings";
+        String id = "JB3Q5";
+        String author = "Vihavainen";
+        String title = "Clean socks";
+        String booktitle = "Laundry for dummies";
+        String year = "1994";
+        String publisher = "jokuvaan";
+        String tulos;
+
+        Parseri parser = new Parseri(kirjat);
+        parser.bibTexTulos = "";
+        tulos = parser.muutaBibTexMuotoonInproceedings(type, id, author, title, booktitle, year, publisher);
+
+
+        String odotettuTulos = "";
+        odotettuTulos = odotettuTulos.concat("@" + type + "{");
+        odotettuTulos = odotettuTulos.concat(id + ", \n");
+        odotettuTulos = odotettuTulos.concat("  author = {" + author + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  title = {" + title + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  booktitle = {" + booktitle + "}, \n");;
+        odotettuTulos = odotettuTulos.concat("  year = {" + year + "}, \n");
+        odotettuTulos = odotettuTulos.concat("  publisher = {" + publisher + "}, \n");
+        odotettuTulos = odotettuTulos.concat("} \n");
+
+        assertEquals(odotettuTulos, tulos);
+    }
     @Test
     public void lueHashmapistaArvoja() {
         List<HashMap> kirjat = new ArrayList();
         HashMap<String, String> kirja = new HashMap();
         kirja.put("type", "book");
+        kirja.put("id", "ABCD");
         kirja.put("author", "vihavainen");
         kirja.put("title", "clean code");
         kirja.put("year", "2008");
+        kirja.put("publisher", "2008");
         kirjat.add(kirja);
-//         Parseri parser = new Parseri(kirjat,1);
-//         System.out.println(parser.getBibTex());
+//        Parseri parser = new Parseri(kirjat);
+//        System.out.println(parser.getBibTex());
     }
 }
